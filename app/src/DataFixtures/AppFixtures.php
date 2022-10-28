13397e7cfaabafc065359eccfeb94850ae0f327e
ilya <ilyazenq@gmail.com>
2022-10-28 15:15:04 +0800

    Fixtures done

--- app/src/DataFixtures/AppFixtures.php
+++ app/src/DataFixtures/AppFixtures.php
@@ -2,7 +2,9 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Order;
 use App\Entity\Product;
+use Carbon\Carbon;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\DBAL\Driver\IBMDB2\Exception\Factory;
 use Doctrine\Persistence\ObjectManager;
@@ -11,13 +13,47 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        for ($i = 0; $i < 10; $i++) {
-            $customer = new Product();
-            $customer->setTitle('testname');
-            $customer->setPrice($i + 20);
-            $manager->persist($customer);
+        $productIds = [];
+        $productPrices = [];
+
+        for ($i = 0; $i < 100; $i++) {
+            $product = new Product();
+            $product->setTitle('product ' . $i);
+            $product->setPrice(mt_rand(10, 100));
+            $manager->persist($product);
+            $manager->flush();
+            $productIds[$product->getId()] = $product->getId();
+            $productPrices[$product->getId()] = $product->getPrice();
+        }
+        $dateBegin = new Carbon();
+        for ($i = 0; $i < 500; $i++) {
+            $count1 = mt_rand(1, 10);
+            $count2 = mt_rand(1, 10);
+            $id1 = array_rand($productIds, 1);
+            $id2 = array_rand($productIds, 1);
+
+            $order = new Order();
+            $order->setApproved(true);
+            $order->setApprovedAt($dateBegin->addHours($i));
+            $order->setProducts([
+                    [
+                        'id' => $id1,
+                        'count' => $count1,
+                        'price_unit' => $productPrices[$id1]
+                    ],
+                    [
+                        'id' => $id2,
+                        'count' => $count2,
+                        'price_unit' => $productPrices[$id2]
+                    ],
+                ]
+            );
+            $order->setPrice($count1 * $productPrices[$id1] + $count2 * $productPrices[$id2]);
+            $order->setTotalCount($count1+$count2);
+
+            $manager->persist($order);
+            $manager->flush();
         }
 
-        $manager->flush();
     }
 }
